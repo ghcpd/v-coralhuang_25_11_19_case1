5044f1ccf34254412fa2c736835c41c9cde8b421
Coral Huang (Centific Technologies Inc) <v-coralhuang@microsoft.com>
2025-11-19 10:36:03 +0800

    input data

---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+// pages/_document.tsx
+import * as React from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+import {
+  DocumentHeadTags,
+  documentGetInitialProps,
+} from '@mui/material-nextjs/v15-pagesRouter';
+import theme from '../src/theme';
+
+export default class MyDocument extends Document {
+  render() {
+    // "props" is intentionally NOT passed into DocumentHeadTags (bug)
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="emotion-insertion-point" content="" />
+          <DocumentHeadTags />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
